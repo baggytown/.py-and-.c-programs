1f07a47e3c23a63c9db9a14fbf0df88b00b0c932
baggytown <codingpython699@gmail.com>
2026-06-12 21:26:30 -0400

    Added more programs

---
+++ Functions.py
@@ -0,0 +1,5 @@
+#Create a function to square a number
+def Square(i):
+    print(f"The square root of {i} is {i*i}")
+num=int(input("Enter a number: "))
+Square(num)
---
+++ Functionss.py
@@ -0,0 +1,7 @@
+#Make function to cheak if even or odd
+def Evdd(i):
+    if i %2 == 0:
+        print("It is even")
+    else:
+        print("It is odd")
+Evdd(i=int(input("Enter a number")))
