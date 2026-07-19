6a200d78501ed3d0af1da9276d11a7ba9c679068
baggytown <codingpython699@gmail.com>
2026-07-19 12:37:21 -0400

    Almost done!

---
+++ Try and Except/login.py
@@ -0,0 +1,8 @@
+while end <=10:
+    try:
+        print("Please enter a string")
+        u=input("Please enter your Username or Email:")
+        p=input("Enter your password:")
+        print("Would you like to change your username and password,then type in 1")
+        print("If you would like to exit please enter 1 ")
+
